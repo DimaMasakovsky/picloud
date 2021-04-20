8d5384ee613ae7c313a5de67dc476b05f212bf60
Masakouski Dzmitry Uladzimirovich <dimon28hbk@mail.ru>
2021-04-20 17:40:29 +0300

    third try

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDyP1Z7JpirT37wrJNb7GdjFhla-c86rcA",
+    authDomain: "picloud-project.firebaseapp.com",
+    projectId: "picloud-project",
+    storageBucket: "picloud-project.appspot.com",
+    messagingSenderId: "18765484938",
+    appId: "1:18765484938:web:99caa64adc79a06bbfee73"
+  }
 };
